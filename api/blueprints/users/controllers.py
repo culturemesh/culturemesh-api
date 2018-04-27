6605808fc0ce7bc443f5aa8fcce26f47923c8164
DrewGregory <djgregny@gmail.com>
2018-04-27 14:14:10 -0700

    forgot %s

--- api/blueprints/users/controllers.py
+++ api/blueprints/users/controllers.py
@@ -80,7 +80,7 @@ def get_user_networks(user_id):
     request_count = request.args["count"]
     count = request_count if request_count is not None else 100
     reg_cursor = connection.cursor()
-    reg_cursor.execute("SELECT id_network FROM network_registration WHERE id_user=%s AND id_network<=",
+    reg_cursor.execute("SELECT id_network FROM network_registration WHERE id_user=%s AND id_network<=%s",
                        (user_id, request.args["max_id"]))
     network_ids = reg_cursor.fetchall()
     reg_cursor.close()

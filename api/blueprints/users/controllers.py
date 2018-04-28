ca36e94969745cf8c054604e6b0278c71ef35f42
DrewGregory <djgregny@gmail.com>
2018-04-28 02:08:30 -0700

    max_id support for user events

--- api/blueprints/users/controllers.py
+++ api/blueprints/users/controllers.py
@@ -124,8 +124,12 @@ def get_user_events(user_id):
     connection = mysql.get_db()
     request_count = int(request.args.get("count", 100))
     event_registration_cursor = connection.cursor()
-    event_registration_cursor.execute("SELECT id_event FROM event_registration WHERE job=%s AND id_guest=%s",
-                                      (request.args["role"], user_id))
+    sql_statement = "SELECT id_event FROM event_registration WHERE job=%s AND id_guest=%s"
+    if "max_id" in request.args:
+        sql_statement += " AND id <= %s"
+        event_registration_cursor.execute(sql_statement, (request.args["role"], user_id, request.args["max_id"]))
+    else:
+        event_registration_cursor.execute(sql_statement, (request.args["role"], user_id))
     event_ids = event_registration_cursor.fetchmany(request_count)
     event_registration_cursor.close()
     event_cursor = connection.cursor()

aa204ff6d9e6eac1681ec4ce0f2f06512b715859
DrewGregory <djgregny@gmail.com>
2018-05-05 00:41:46 -0700

    Forgot extend did not return new copy.

--- api/blueprints/users/controllers.py
+++ api/blueprints/users/controllers.py
@@ -26,16 +26,17 @@ def users_query():
     connection = mysql.get_db()
     # Parse id's into collection
     near_ids = request.args["near_location"].split(",")
-    print(near_ids is not None)
     network_cursor = connection.cursor()
     near_loc_query = "id_city_cur=%s AND id_region_cur=%s AND id_country_cur=%s"
     if "language" in request.args:
+        near_ids.extend([str(request.args["language"])])
         network_cursor.execute("SELECT * FROM networks WHERE " + near_loc_query + " AND id_language_origin=%s",
-                               tuple(near_ids.extend([str(request.args["language"])])))
+                               tuple(near_ids))
     elif "from_location" in request.args:
+        near_ids.extend(request.args["from_location"].split(","))
         network_cursor.execute("SELECT * FROM networks WHERE " + near_loc_query + " AND " +
                                near_loc_query.replace("cur", "origin"),
-                               tuple(near_ids.extend(request.args["from_location"].split(","))))
+                               tuple(near_ids))
     else:
         return make_response("No language/from location", HTTPStatus.METHOD_NOT_ALLOWED)
     network_ids = network_cursor.fetchall()

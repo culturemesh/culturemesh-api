4425f9a840cc9889a39d354550758a2490903f7a
Alan <alanf94@stanford.edu>
2018-05-10 22:45:26 -0700

    get network posts

--- api/blueprints/networks/controllers.py
+++ api/blueprints/networks/controllers.py
@@ -15,3 +15,28 @@ def test():
 @require_apikey
 def get_network(network_id):
     return get_by_id("networks", network_id)
+
+@networks.route("/<network_id>/posts", methods=["GET"])
+@require_apikey
+def get_network_posts(network_id):
+    conn = mysql.get_db()
+    count = int(request.args.get("count", 100))
+    cursor = conn.cursor()
+    query = "SELECT * \
+             FROM posts \
+             WHERE id_network=%s"
+    order = "ORDER BY id DESC"
+    if "max_id" in request.args:
+      max_id = request.args["max_id"]
+      query += " AND id <= %s"
+      cursor.execute(query + order, (network_id, max_id))
+    else:
+      cursor.execute(query + order, (network_id,))
+    posts = cursor.fetchmany(count)
+
+    if len(posts) == 0:
+      return make_response(jsonify([]), HTTPStatus.OK)
+
+    posts = convert_objects(posts, cursor.description)
+    cursor.close()
+    return make_response(jsonify(posts), HTTPStatus.OK)

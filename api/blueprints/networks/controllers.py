1797fdc6e13e1f82b0a46465c07f0ef88fd6200d
Alan <alanf94@stanford.edu>
2018-05-10 20:16:34 -0700

    fix typo

--- api/blueprints/networks/controllers.py
+++ api/blueprints/networks/controllers.py
@@ -22,7 +22,7 @@ def get_network(network_id):
 
     network = network_cursor.fetchone()
     if network is not None:
-        network = convert_objects([network], user_cursor.description)[0]
+        network = convert_objects([network], network_cursor.description)[0]
     network_cursor.close()
-    return make_response(jsonify(user),
+    return make_response(jsonify(network),
       HTTPStatus.METHOD_NOT_ALLOWED if network is None else HTTPStatus.OK)

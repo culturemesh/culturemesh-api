d870c03392d5e1fc83d46999d155abe0a9249f6f
Alan <alanf94@stanford.edu>
2018-05-10 21:43:22 -0700

    get events

--- api/blueprints/events/controllers.py
+++ api/blueprints/events/controllers.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, json, make_response
 from api import require_apikey
+from http import HTTPStatus
+from api.extensions import mysql
+from api.apiutils import *
 
 events = Blueprint('event', __name__)
 
@@ -7,3 +10,18 @@ events = Blueprint('event', __name__)
 @require_apikey
 def test():
     return "pong"
+
+@events.route("/<event_id>", methods=["GET"])
+@require_apikey
+def get_event(event_id):
+    connection = mysql.get_db()
+    events_cursor = connection.cursor()
+
+    events_cursor.execute('SELECT * '
+                         'FROM events '
+                         'WHERE id=%s', (post_id,))
+
+    response = make_response_from_single_tuple(events_cursor)
+    events_cursor.close()
+    return response
+

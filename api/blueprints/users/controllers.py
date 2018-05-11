c999aa85e0e4f1aaf9b41b111cfca62ba1703b92
Alan <alanf94@stanford.edu>
2018-05-11 12:08:42 -0700

    get user events logic abstracted

--- api/blueprints/users/controllers.py
+++ api/blueprints/users/controllers.py
@@ -119,7 +119,7 @@ def get_user_events(user_id):
                           FROM event_registration \
                           INNER JOIN events \
                           ON events.id = event_registration.id_event \
-                          WHERE id_guest=%s AND job=%s",
+                          WHERE event_registration.id_guest=%s AND event_registration.job=%s",
                           selection_ids=[user_id, request.args["role"]],
                           args=request.args,
                           order_clause="ORDER BY id_event DESC",

ab8e32840b3f175af0f42f28fd4438d357e977b2
Alan <alanf94@stanford.edu>
2018-05-11 16:04:28 -0700

    added event registration event

--- api/blueprints/events/controllers.py
+++ api/blueprints/events/controllers.py
@@ -16,3 +16,14 @@ def test():
 def get_event(event_id):
     return get_by_id("events", event_id)
 
+@events.route("/<event_id>/reg", methods=["GET"])
+@require_apikey
+def get_event_registration(event_id):
+    return get_paginated("SELECT * \
+                          FROM event_registration \
+                          WHERE id_event=%s",
+                          selection_fields=[event_id],
+                          args=request.args,
+                          order_clause="ORDER BY date_registered DESC",
+                          order_index_format="date_registered <= %s",
+                          order_arg="max_registration_date")

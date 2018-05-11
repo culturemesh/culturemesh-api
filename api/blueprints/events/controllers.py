65ca08b42d2d102472372330bc2cb18462c789a6
Alan <alanf94@stanford.edu>
2018-05-11 15:59:08 -0700

    get event refactored

--- api/blueprints/events/controllers.py
+++ api/blueprints/events/controllers.py
@@ -14,14 +14,5 @@ def test():
 @events.route("/<event_id>", methods=["GET"])
 @require_apikey
 def get_event(event_id):
-    connection = mysql.get_db()
-    events_cursor = connection.cursor()
-
-    events_cursor.execute('SELECT * '
-                         'FROM events '
-                         'WHERE id=%s', (event_id,))
-
-    response = make_response_from_single_tuple(events_cursor)
-    events_cursor.close()
-    return response
+    return get_by_id("events", event_id)
 

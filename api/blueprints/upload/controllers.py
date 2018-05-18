623b6a7f981ea6e7f28d36a62cc73e425d6f64e9
DrewGregory <djgregny@gmail.com>
2018-05-18 02:35:56 -0700

    Beginnings of image upload.

--- api/blueprints/upload/controllers.py
+++ api/blueprints/upload/controllers.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response
+from http import HTTPStatus
 from api import require_apikey
 
 
@@ -9,3 +10,20 @@ upload = Blueprint('upload', __name__)
 @require_apikey
 def test():
     return "pong"
+
+
+@upload.route("/image", methods=["POST"])
+@require_apikey
+def upload_image():
+    """
+    This function uploads the image to the BlueHost filesystem. The request body should be multipart/formdata,
+    with only one key-value pair: key - 'file'       value - image binary
+    :return: The request will return the url
+    """
+    if request.files is None:
+        return make_response("No image in request body.", HTTPStatus.METHOD_NOT_ALLOWED)
+    
+
+    # TODO: Fetch image binary
+    # TODO: Upload image to file system.
+    # TODO: Return new url.

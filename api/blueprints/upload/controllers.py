f4d67a0394a3ba9e1b308095b628ddbf72b8720b
DrewGregory <djgregny@gmail.com>
2018-05-18 21:34:35 -0700

    conform to website filesaving conventions.

--- api/blueprints/upload/controllers.py
+++ api/blueprints/upload/controllers.py
@@ -10,6 +10,8 @@ from werkzeug.utils import secure_filename
 
 upload = Blueprint('upload', __name__)
 
+# Our buffer size for file reads is
+BUF_SIZE = 2<<(10 * 3)
 
 @upload.route("/ping")
 @require_apikey
@@ -35,9 +37,28 @@ def upload_image():
     file = request.files['file']
     # We need to safeguard against mischievous file names.
     file_name = secure_filename(file.filename)
+    # Now, let's hash this image to get the directory name.
+    directory_name = hash_file(file)
     # Upload image to file system.
-    path = os.path.join(host_path['image_uploads'], hashlib.md5(file_name.encode('utf-8')).hexdigest() +
-                        "." + str(file_name).split(".")[-1])
+    # Make hash directory
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+    path = os.path.join(host_path['image_uploads'], directory_name + '/' + file_name)
     file.save(path)
     # Return new url.
     return path
+
+
+def hash_file(file):
+    """
+    Generates a string hex hash (using md5) of the image file. We use a buffer to separate the file into
+    memory-manageable chunks.
+    :param file: should be a python file.
+    :return: string of hash in hex.
+    """
+    md5 = hashlib.md5()
+    data = file.read(BUF_SIZE)
+    while data:
+        md5.update(data)
+        data = file.read(BUF_SIZE)
+    return md5.hexdigest()

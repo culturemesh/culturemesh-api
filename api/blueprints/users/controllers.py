1dba34e8b11e808003f784d885a84fdc06893694
Alan <alanf94@stanford.edu>
2018-05-20 13:02:43 -0700

    put user

--- api/blueprints/users/controllers.py
+++ api/blueprints/users/controllers.py
@@ -97,7 +97,7 @@ def handle_users_put(request):
     query += " WHERE id=%s"
     args.append(content['id'])
 
-    return make_response(query, HTTPStatus.OK)
+    return make_response(query + str(args), HTTPStatus.OK)
     execute_insert(query, tuple(args))
     return make_response("OK", HTTPStatus.OK)
 

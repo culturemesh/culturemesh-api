1960b4e055c818b18402ed62367304940591a887
DrewGregory <djgregny@gmail.com>
2018-07-09 00:23:17 -0700

    testing.

--- api/blueprints/networks/controllers.py
+++ api/blueprints/networks/controllers.py
@@ -103,7 +103,7 @@ def get_networks():
             return make_response("Invalid network parameters. Could not make a new network.",
                                  HTTPStatus.METHOD_NOT_ALLOWED)
     else:
-        return response
+        return make_response("In here???")
 
 
 @networks.route("/<network_id>", methods=["GET"])

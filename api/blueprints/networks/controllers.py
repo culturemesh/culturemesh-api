d7971f81426fe7b2f19635600150466a28ce4f04
DrewGregory <djgregny@gmail.com>
2018-07-08 18:10:56 -0700

    Checking how response object works.

--- api/blueprints/networks/controllers.py
+++ api/blueprints/networks/controllers.py
@@ -33,7 +33,7 @@ def get_networks():
                              order_clause="ORDER BY id DESC",
                              order_index_format="id <= %s",
                              order_arg="max_id")
-        if response.get_json() == jsonify([]):
+        if response.json == jsonify([]):
             return make_response(jsonify({"param": "oooh I can do stuff with this check."}))
         return response
     elif "language" in request.args:

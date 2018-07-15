9923ab749804ce42bd6022e10fb345e8546e685a
DrewGregory <djgregny@gmail.com>
2018-07-15 00:43:09 -0700

    updating user object.

--- api/blueprints/accounts/controllers.py
+++ api/blueprints/accounts/controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, g
 from hashlib import md5
-from api.apiutils import get_by_id, convert_objects
+from api.apiutils import convert_objects
 from flask_httpauth import HTTPBasicAuth
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
@@ -75,7 +75,7 @@ class User:
             return None  # valid token, but expired
         except BadSignature:
             return None  # invalid token
-        return get_by_id("users", data["id"])
+        return get_user_by_id("users", data["id"])
 
 
 def get_user_by_username(username):
@@ -90,6 +90,26 @@ def get_user_by_username(username):
     # need to escape it.
     query = "SELECT * FROM users WHERE username=%s"
     cursor.execute(query, (username,))
+    user_db_tuple = cursor.fetchone()
+    if user_db_tuple is None:
+        return None
+    user = convert_objects([user_db_tuple], cursor.description)[0]
+    cursor.close()
+    return user
+
+
+def get_user_by_id(id):
+    """
+    Checks database and returns object representing user with that id.
+    :param id: id of CultureMesh account (string)
+    :return: user_obj from db or None if no corresponding found.
+    """
+    connection = mysql.get_db()
+    cursor = connection.cursor()
+    # Note table_name is never supplied by a client, so we do not
+    # need to escape it.
+    query = "SELECT * FROM users WHERE id=%s"
+    cursor.execute(query, (id,))
     user_db_tuple = cursor.fetchone()
     if user_db_tuple is None:
         return None

71c4c534380fec007cc75d36ab1b95f3da0e9755
DrewGregory <djgregny@gmail.com>
2018-08-12 18:00:23 -0700

    messed up sql

--- api/blueprints/events/utils.py
+++ api/blueprints/events/utils.py
@@ -14,6 +14,6 @@ def get_event_id(user_id, network_id):
     """
     connection = mysql.get_db()
     cursor = connection.cursor()
-    cursor.execute("SELECT id FROM events WHERE id_host=%s AND id_=%s ORDER BY id DESC LIMIT 1",
+    cursor.execute("SELECT id FROM events WHERE id_host=%s AND id=%s ORDER BY id DESC LIMIT 1",
                    (user_id, network_id))
     return convert_objects([cursor.fetchone()], cursor.description)[0]["id"]

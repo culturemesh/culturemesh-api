6c3ba59aa3dbfe17ad6412739d1c9587f638ad85
DrewGregory <djgregny@gmail.com>
2018-08-27 23:52:45 -0700

    #62 comments

--- api/blueprints/networks/controllers.py
+++ api/blueprints/networks/controllers.py
@@ -265,6 +265,6 @@ def popular():
         return make_response("Invalid count parameter", HTTPStatus.BAD_REQUEST)
     # For some reason, distinct only works on individual columns, so we will have to first just get the ids.
     networks_cursor = connection.cursor()
-    networks_cursor.execute("select * from networks order by (select COUNT(*) from network_registration where id=id_network)\
-            desc limit %s", (count,))
+    networks_cursor.execute("SELECT * FROM networks ORDER BY (SELECT COUNT(*) FROM network_registration WHERE id=id_network)\
+            DESC limit %d", (count,))
     return make_response(jsonify(convert_objects(networks_cursor.fetchall(), networks_cursor.description)), HTTPStatus.OK)

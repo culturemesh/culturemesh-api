e620bbcd74366c255af884953ffd41f26e02749b
Alan <alanf94@stanford.edu>
2018-09-19 13:32:37 -0600

    Add security HTTP headers to API responses

--- api/__init__.py
+++ api/__init__.py
@@ -31,3 +31,13 @@ api.register_blueprint(locations, url_prefix='/location')
 api.register_blueprint(languages, url_prefix='/language')
 api.register_blueprint(accounts, url_prefix='/account')
 api.register_blueprint(upload, url_prefix='/upload')
+
+@api.after_request
+def add_custom_http_response_headers(response):
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["Strict-Transport-Security"] = "max-age=86400; includeSubDomains"
+    response.headers["Expires"] = "Thu, 01 Jan 1970 00:00:00 GMT"
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, max-age=0"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["X-CultureMesh"] = "API"
+    return response

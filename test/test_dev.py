21b82fa9ece4e51e1e48402cff5633f460d48509
cs <cs.temporary@icloud.com>
2018-12-14 02:00:48 +0000

    Add hypothesis test for /dev/note

--- test/test_dev.py
+++ test/test_dev.py
@@ -1,3 +1,8 @@
+from hypothesis import given
+from hypothesis.strategies import text
+from hypothesis import assume
+from test import client
+
 """Tests for developer endpoints defined in :py:class:`api.controllers.dev`
 
 """
@@ -17,3 +22,11 @@ def test_note_returns_file_contents(client):
     with open(client.application.config["NOTE_PATH"], "w") as note_file:
         note_file.write(NOTE_TEST_STRING)
     assert client.get("/dev/note").data.decode() == NOTE_TEST_STRING
+
+
+@given(test_string=text())
+def test_note_returns_any_file_contents(client, test_string):
+    assume('\r' not in test_string)
+    with open(client.application.config["NOTE_PATH"], "w") as note_file:
+        note_file.write(test_string)
+    assert client.get("/dev/note").data.decode() == test_string

9c1f1063f0c574f9bcb52182ae1ac5a1e1e9f3a6
cs <cs.temporary@icloud.com>
2018-12-16 19:03:51 +0000

    Add unit test for getting user by ID

--- test/unit/test_users.py
+++ test/unit/test_users.py
@@ -3,6 +3,7 @@ from api import credentials
 import json
 import mock
 from hashlib import md5
+import datetime
 
 
 user_def = {"username": "MyUserName3!",
@@ -79,3 +80,52 @@ def test_create_user_email_taken_fail(user_by_email, user_by_username,
     execute_insert.assert_not_called()
     assert response.status_code == 400
     assert response.data.decode() == "Username already taken or invalid params"
+
+
+description = (('id', 8, None, 20, 20, 0, False),
+               ('username', 253, None, 30, 30, 0, True),
+               ('first_name', 253, None, 30, 30, 0, True),
+               ('last_name', 253, None, 30, 30, 0, True),
+               ('email', 253, None, 50, 50, 0, False),
+               ('password', 253, None, 32, 32, 0, True),
+               ('role', 2, None, 1, 1, 0, True),
+               ('register_date', 7, None, 19, 19, 0, False),
+               ('last_login', 7, None, 19, 19, 0, False),
+               ('gender', 253, None, 1, 1, 0, True),
+               ('about_me', 253, None, 500, 500, 0, True),
+               ('events_upcoming', 2, None, 1, 1, 0, True),
+               ('events_interested_in', 2, None, 1, 1, 0, True),
+               ('company_news', 2, None, 1, 1, 0, True),
+               ('network_activity', 2, None, 1, 1, 0, True),
+               ('confirmed', 1, None, 1, 1, 0, False),
+               ('act_code', 253, None, 32, 32, 0, False),
+               ('img_link', 253, None, 50, 50, 0, True),
+               ('fp_code', 254, None, 96, 96, 0, True))
+
+sql_object = (2, 'CYoum23', 'Chris', 'Youm', 'upperbrain@gmail.com',
+             'b53a15ae0b7d18f359dd0f5e0fa9cc7b', 0,
+             datetime.datetime(2018, 7, 30, 23, 45, 5),
+             datetime.datetime(2014, 6, 1, 11, 54, 27), 'M',
+             'I am from Korea and working at SF as game developer.', 1, 1,
+             1, 1, 1, '97657a61f23bff2b65c63c9aacf4f032',
+             '1401652610_0/pp.png', None)
+
+
+@mock.patch("api.apiutils.execute_get_one",
+            return_value=(sql_object, description))
+def test_get_user_by_id(get_one, client):
+    response = client.get("/user/2",
+                          query_string={"key": credentials.api["key"]})
+    query = "SELECT * FROM `users` WHERE id=%s"
+    get_one.assert_called_with(query, '2')
+    assert response.status_code == 200
+    exp = {'about_me': 'I am from Korea and working at SF as game developer.',
+           'act_code': '97657a61f23bff2b65c63c9aacf4f032', 'company_news': 1,
+           'confirmed': 1, 'events_interested_in': 1, 'events_upcoming': 1,
+           'first_name': 'Chris', 'fp_code': None, 'gender': 'M', 'id': 2,
+           'img_link': '1401652610_0/pp.png',
+           'last_login': 'Sun, 01 Jun 2014 11:54:27 GMT', 'last_name': 'Youm',
+           'network_activity': 1,
+           'register_date': 'Mon, 30 Jul 2018 23:45:05 GMT', 'role': 0,
+           'username': 'CYoum23'}
+    assert response.json == exp

040980aa9066d73b395489b6fb5094bbe0cccdfd
cs <cs.temporary@icloud.com>
2018-12-19 23:40:59 +0000

    Add unit test for joining event

--- test/unit/test_users.py
+++ test/unit/test_users.py
@@ -241,3 +241,23 @@ def test_update_user(execute_insert, auth, get_id, client):
     auth.assert_called_with(None, None)
     assert response.status_code == 200
     assert response.data.decode() == "OK"
+
+
+@mock.patch('api.blueprints.users.controllers.get_curr_user_id', return_value=1)
+@mock.patch('api.blueprints.accounts.controllers.auth.authenticate',
+            return_value=True)
+@mock.patch('api.blueprints.users.controllers.event_exists', return_value=True)
+@mock.patch('api.blueprints.users.utils.execute_insert')
+def test_join_event(execute_insert, event_exists, auth, get_id, client):
+    response = client.post('/user/joinEvent/23',
+                           query_string={'key': credentials.api['key'],
+                                         'role': 'guest'})
+    query = "INSERT INTO event_registration VALUES " \
+            "(%s,%s,CURRENT_TIMESTAMP, %s)"
+    args = (1, '23', 'guest')
+    execute_insert.assert_called_with(query, args)
+    event_exists.assert_called_with('23')
+    get_id.assert_called_with()
+    auth.assert_called_with(None, None)
+    assert response.status_code == 200
+    assert response.data.decode() == 'OK'

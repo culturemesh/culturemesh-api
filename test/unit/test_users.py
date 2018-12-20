0e920f74cb2be35e1b31cac4cbb7af16893d8320
cs <cs.temporary@icloud.com>
2018-12-20 20:55:59 +0000

    Add unit test for getting the users of a network

--- test/unit/test_users.py
+++ test/unit/test_users.py
@@ -5,6 +5,7 @@ import mock
 from hashlib import md5
 import datetime
 from pymysql.err import IntegrityError
+from mock import call
 
 
 def test_ping(client):
@@ -437,3 +438,100 @@ def test_leave_network(execute_mod, auth, get_id, client):
     execute_mod.assert_called_with(query, args)
     assert response.status_code == 200
     assert response.data.decode() == 'User 1 left network 2'
+
+
+# execute_get_all
+get_net_users_net_obj = ((3161,),)
+get_net_users_net_des = (('id', 8, None, 20, 20, 0, False),)
+
+# execute_get_many
+get_net_users_reg_obj = ((178,),)
+get_net_users_reg_des = (('id_user', 8, None, 20, 20, 0, False),)
+
+# execute_get_all
+get_net_users_usr_obj = ((178, 'sbdbb', 'dndn', 'dbdn', 'snsj',
+                          '098f6bcd4621d373cade4e832627b4f6', None,
+                          datetime.datetime(2018, 8, 21, 23, 36, 5),
+                          '0000-00-00 00:00:00', None, None, None, None, None,
+                          None, 0, '', None, None),)
+get_net_users_usr_des = (('id', 8, None, 20, 20, 0, False),
+                         ('username', 253, None, 30, 30, 0, True),
+                         ('first_name', 253, None, 30, 30, 0, True),
+                         ('last_name', 253, None, 30, 30, 0, True),
+                         ('email', 253, None, 50, 50, 0, False),
+                         ('password', 253, None, 32, 32, 0, True),
+                         ('role', 2, None, 1, 1, 0, True),
+                         ('register_date', 7, None, 19, 19, 0, False),
+                         ('last_login', 7, None, 19, 19, 0, False),
+                         ('gender', 253, None, 1, 1, 0, True),
+                         ('about_me', 253, None, 500, 500, 0, True),
+                         ('events_upcoming', 2, None, 1, 1, 0, True),
+                         ('events_interested_in', 2, None, 1, 1, 0, True),
+                         ('company_news', 2, None, 1, 1, 0, True),
+                         ('network_activity', 2, None, 1, 1, 0, True),
+                         ('confirmed', 1, None, 1, 1, 0, False),
+                         ('act_code', 253, None, 32, 32, 0, False),
+                         ('img_link', 253, None, 50, 50, 0, True),
+                         ('fp_code', 254, None, 96, 96, 0, True))
+
+
+def mock_get_all_get_net_users(sql_q_format, args):
+    if 'FROM networks' in sql_q_format:
+        return get_net_users_net_obj, get_net_users_net_des
+    elif 'FROM users' in sql_q_format:
+        return get_net_users_usr_obj, get_net_users_usr_des
+    else:
+        raise ValueError('sql_q_format should specify either from the networks'
+                         'or from the users table.')
+
+
+@mock.patch('api.blueprints.users.controllers.execute_get_many',
+            return_value=(get_net_users_reg_obj, get_net_users_reg_des))
+@mock.patch('api.blueprints.users.controllers.execute_get_all',
+            side_effect=mock_get_all_get_net_users)
+def test_get_net_users(get_all, get_many, client):
+    city_cur = 327181
+    region_cur = 56130
+    country_cur = 47228
+    near_loc = str(country_cur) + ',' + str(region_cur) + ',' + str(city_cur)
+
+    city_origin = 332851
+    region_origin = 55833
+    country_origin = 47228
+    from_loc = str(country_origin) + ',' + str(region_origin) + ',' + \
+               str(city_origin)
+
+    response = client.get('/user/users',
+                          query_string={'key': credentials.api['key'],
+                                        'near_location': near_loc,
+                                        'from_location': from_loc})
+
+    net_query = 'SELECT id FROM networks WHERE id_country_cur=%s AND ' \
+                'id_region_cur=%s AND id_city_cur=%s AND ' \
+                'id_country_origin=%s AND id_region_origin=%s AND ' \
+                'id_city_origin=%s'
+    net_args = ('47228', '56130', '327181', '47228', '55833', '332851')
+    net_call = call(net_query, net_args)
+
+    reg_query = 'SELECT id_user FROM network_registration WHERE ' \
+                'id_network IN %sORDER BY id_user DESC'
+    reg_args = (((3161,),),)
+    reg_count = 100
+    get_many.assert_called_with(reg_query, reg_args, reg_count)
+
+    usr_query = 'SELECT * FROM users WHERE id IN %s'
+    usr_args = (((178,),),)
+    usr_call = call(usr_query, usr_args)
+
+    get_all.assert_has_calls([net_call, usr_call], any_order=False)
+
+    assert response.status_code == 200
+    exp = [{'about_me': None, 'act_code': '', 'company_news': None,
+            'confirmed': 0, 'events_interested_in': None,
+            'events_upcoming': None, 'first_name': 'dndn', 'fp_code': None,
+            'gender': None, 'id': 178, 'img_link': None,
+            'last_login': '0000-00-00 00:00:00', 'last_name': 'dbdn',
+            'network_activity': None,
+            'register_date': 'Tue, 21 Aug 2018 23:36:05 GMT', 'role': None,
+            'username': 'sbdbb'}]
+    assert response.json == exp

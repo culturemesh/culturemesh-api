1cc164c2e2bb27005f7535a4c547d465be20c154
cs <cs.temporary@icloud.com>
2018-12-24 22:14:08 +0000

    Refactor events controllers for style and to use get_curr_user_id

--- api/blueprints/events/controllers.py
+++ api/blueprints/events/controllers.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, request, g
-
+from flask import Blueprint, request
 from api.blueprints.accounts.controllers import auth
-from api.blueprints.users.utils import _add_user_to_event
+from api.blueprints.users.utils import _add_user_to_event, get_curr_user_id
 from api.apiutils import *
 
 events = Blueprint('event', __name__)
@@ -10,6 +9,7 @@ events = Blueprint('event', __name__)
 @events.route("/ping")
 def test():
     return "pong"
+
 
 @events.route("/<event_id>", methods=["GET"])
 def get_event(event_id):
@@ -27,6 +27,7 @@ def get_event_registration(event_id):
                           order_index_format="date_registered <= %s",
                           order_arg="max_registration_date")
 
+
 @events.route("/<event_id>/reg_count", methods=["GET"])
 def get_event_registration_count(event_id):
     query = "SELECT count(*) \
@@ -40,14 +41,14 @@ def get_event_registration_count(event_id):
 @auth.login_required
 def make_new_event():
     req_obj = make_fake_request_obj(request)
-    req_obj.form["id_host"] = g.user.id
+    req_obj.form["id_host"] = get_curr_user_id()
     if request.method == 'POST':
         # POST
-        content_fields = ['id_network', 'id_host', \
-                'event_date', 'title', \
-                'address_1', 'address_2', \
-                'country', 'city', \
-                'region', 'description']
+        content_fields = ['id_network', 'id_host',
+                          'event_date', 'title',
+                          'address_1', 'address_2',
+                          'country', 'city',
+                          'region', 'description']
         response = execute_post_by_table(request, content_fields, "events")
         # Unfortunately, we have to get the event id
         content = request.get_json()
@@ -55,7 +56,8 @@ def make_new_event():
             content = request.form
         connection = mysql.get_db()
         cursor = connection.cursor()
-        cursor.execute("SELECT id FROM events WHERE id_host=%s AND id_network=%s ORDER BY id DESC LIMIT 1",
+        cursor.execute("SELECT id FROM events WHERE id_host=%s AND id_"
+                       "network=%s ORDER BY id DESC LIMIT 1",
                        (content["id_host"], content["id_network"]))
         obj = cursor.fetchone()
         event_id = convert_objects([obj], cursor.description)[0]["id"]
@@ -67,29 +69,31 @@ def make_new_event():
         # Check if user is authorized to update this event
         event = get_by_id("events", req_obj.form["id"], [])
         event = get_response_content_as_json(event)
-        if event and "id_host" in event and event["id_host"] == g.user.id:
+        if event and "id_host" in event and event["id_host"] == \
+                get_curr_user_id():
             return execute_put_by_id(req_obj, "events")
 
 
 @events.route("/currentUserEventsByNetwork/<network_id>", methods=["GET"])
 @auth.login_required
 def user_events_for_network(network_id):
-    user_id = g.user.id
+    user_id = get_curr_user_id()
     return get_paginated("SELECT * \
                          FROM event_registration INNER JOIN events ON events.id = event_registration.id_event \
                          WHERE (id_guest=%s OR id_host=%s) AND id_network=%s",
                          selection_fields=[user_id, user_id, network_id],
-                         args= request.args,
+                         args=request.args,
                          order_clause="ORDER BY id DESC",
                          order_index_format="id <= %s",
                          order_arg="id")
+
 
 @events.route("/delete", methods=["DELETE"])
 @auth.login_required
 def delete_event():
     event_id = request.args.get('id')
     if not event_id or not event_id.isdigit():
-      return make_response("Invalid Input", HTTPStatus.BAD_REQUEST)
+        return make_response("Invalid Input", HTTPStatus.BAD_REQUEST)
 
     connection = mysql.get_db()
     cursor = connection.cursor()
